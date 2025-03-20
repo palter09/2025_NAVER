243ad39b808b0d5ecd6a62427986a99b4a0b060b
palter09 <42296211+palter09@users.noreply.github.com>
2025-03-20 14:31:18 +0900

    Update 14215번 세 막대.cpp

--- 기하: 직사각형과 삼각형/14215번 세 막대.cpp
+++ 기하: 직사각형과 삼각형/14215번 세 막대.cpp
@@ -7,13 +7,13 @@ int main()
 {
 	int a, b, c;
 	cin >> a >> b >> c;
-	
+
 	vector<int> v;
 	v.push_back(a);
 	v.push_back(b);
 	v.push_back(c);
 	sort(v.begin(), v.end());
-	
+
 	if (v[2] >= v[0] + v[1]) {
 		cout << 2 * (v[0] + v[1]) - 1;
 	}
@@ -21,7 +21,7 @@ int main()
 	else if (v[0] == v[1] && v[0] == v[2] && v[1] == v[2]) {
 		cout << 3 * v[0];
 	}
-	
+
 	else {
 		cout << v[0] + v[1] + v[2];
 	}
