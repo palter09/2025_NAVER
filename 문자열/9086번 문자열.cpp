1ac97a019dc86f0f3cec5a0b87ff4b0513d2aae6
palter09 <42296211+palter09@users.noreply.github.com>
2025-03-20 16:22:22 +0900

    Create 9086번 문자열.cpp

---
+++ 문자열/9086번 문자열.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin >> t;
+
+	for (int i = 0; i < t; i++) {
+		string s;
+		cin >> s;
+		cout << s[0] << s[s.size()-1] << endl;
+	}
+}
