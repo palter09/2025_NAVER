0663a620c7b05778622fc1dd942aba6712775b77
palter09 <42296211+palter09@users.noreply.github.com>
2025-03-20 13:37:12 +0900

    Create 15894 수학은 체육과목 입니다.cpp

---
+++ 기하: 직사각형과 삼각형/15894 수학은 체육과목 입니다.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long n;
+    cin >> n;
+
+    // up = 1;
+    // down = n;
+    // side = (2 * n) + n - 1;
+    // up + down + side = 4n;
+
+    cout << 4*n;
+}
